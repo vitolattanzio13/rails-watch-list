b73a6a586d6ddb748f1736ebf8728b4df0d9fd65
Vito Lattanzio <vlattanzio13@gmail.com>
2022-11-18 11:49:31 +0100

    watch list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -20,13 +20,19 @@ class ListsController < ApplicationController
     redirect_to lists_path(@list)
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
+
   private
 
-    def find_list
-        @list = List.find(params[:id])
-    end
+  def find_list
+    @list = List.find(params[:id])
+  end
 
-    def list_params
-        params.require(:list).permit(:name)
-    end
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :lists, only: [:index, :show, :new, :create] do
+  resources :lists, only: [:index, :show, :new, :create, :destroy] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
